fceea12efdbb5a1fddb09c39ec1a29dbceb49ca9
Akanksha Ashu <Akanksha@gmail.com>
2019-10-18 15:02:59 +0530

    maven project by ashu

---
+++ src/main/java/CaptenMarvel/CaptenMarvel.java
@@ -0,0 +1,10 @@
+package CaptenMarvel;
+
+public class CaptenMarvel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello!CaptainMarvel");
+	}
+
+}
